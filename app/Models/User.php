8584933d82b88f6cabc64e9ecc98ff72c6a1af90
Kaka Praditha <pradithaakaka@gmail.com>
2025-06-07 02:37:27 +0700

    perbaikan: Cegah duplikasi nomor telepon dan tingkatkan keamanan peran

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,35 +11,62 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     * 'role' sengaja dihilangkan dari sini untuk keamanan.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
         'phone',
         'address',
-        'role',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 
+    /**
+     * Get the vehicles for the user.
+     */
     public function vehicles()
     {
         return $this->hasMany(Vehicle::class);
     }
 
+    /**
+     * Get the bookings for the user.
+     */
     public function bookings()
     {
         return $this->hasMany(Booking::class);
     }
 
+    /**
+     * Check if the user is an admin.
+     */
     public function isAdmin(): bool
     {
         return $this->role === 'admin';
